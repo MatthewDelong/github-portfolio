1e448172c928914ead9ac906834dff5842313215
MatthewDelong <matthew.delong@hotmail.co.uk>
2024-05-09 11:33:47 +0100

    anylytics Test 2 removed .env from gitignore

--- data/user-data.js
+++ data/user-data.js
@@ -8,6 +8,6 @@ export const userData = {
   stackOverflow: 'https://stackoverflow.com/users/20139637/matthew-delong',
   leetcode: "https://leetcode.com/u/Matthew_Delong/",
   //resume: "https://drive.google.com/file/d/1eyutpKFFhJ9X-qpQGKhUNnVRkB5Wer00/view?usp=sharing",//
-  skills: ['Bootstrap', 'HTML', 'CSS', 'JS'],
+  skills: ['Bootstrap', 'HTML', 'CSS', 'JS',],
   timezone: '+1'
 };
